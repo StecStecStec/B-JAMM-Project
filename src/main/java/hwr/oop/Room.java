214dc7f1d9ba898cee175bac1bc5995e08321f7d
StecStecStec <007kryspin@gmail.com>
2024-04-10 14:17:27 +0200

    review progress

--- src/main/java/hwr/oop/Room.java
+++ src/main/java/hwr/oop/Room.java
@@ -7,25 +7,22 @@ import java.util.ArrayList;
 public class Room {
 
   private List<Shelf> shelfList;
-
   private int shelfLimit;
-
   private UUID roomID;
 
   public UUID getRoomID() {
     return roomID;
   }
-
   public int getShelfLimit() {
     return shelfLimit;
   }
-
   public List<Shelf> getShelfList() {
     return shelfList;
   }
   public void roomAddShelf(Shelf shelf) {
     shelfList.add(shelf);
   }
+
   public Room() {
     this.shelfList = new ArrayList<Shelf>();
     this.shelfLimit = 5;
