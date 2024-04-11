a3f90fa7bda53ecd647f44a54a031533a6b62418
janfoec <jafoe2@gmail.com>
2024-04-11 09:52:21 +0200

    created Tests for equals and hash method, added setter method 'setRoomID', 'setShelfList'

--- src/main/java/hwr/oop/Room.java
+++ src/main/java/hwr/oop/Room.java
@@ -7,42 +7,55 @@ import java.util.ArrayList;
 
 public class Room {
 
-  private List<Shelf> shelfList;
-  private int shelfLimit;
-  private UUID roomID;
+    private List<Shelf> shelfList;
+    private int shelfLimit;
+    private UUID roomID;
 
-  public UUID getRoomID() {
-    return roomID;
-  }
-  public int getShelfLimit() {
-    return shelfLimit;
-  }
-  public List<Shelf> getShelfList() {
-    return shelfList;
-  }
-  public void roomAddShelf(Shelf shelf) {
-    shelfList.add(shelf);
-  }
-  public void roomRemoveShelf(Shelf shelf) {
-    shelfList.remove(shelf);
-  }
+    public UUID getRoomID() {
+        return roomID;
+    }
 
-  public Room() {
-    this.shelfList = new ArrayList<Shelf>();
-    this.shelfLimit = 5;
-    this.roomID = UUID.randomUUID();
-  }
+    public int getShelfLimit() {
+        return shelfLimit;
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    Room room = (Room) o;
-    return shelfLimit == room.shelfLimit && Objects.equals(shelfList, room.shelfList) && Objects.equals(roomID, room.roomID);
-  }
+    public List<Shelf> getShelfList() {
+        return shelfList;
+    }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(shelfList, shelfLimit, roomID);
-  }
+    public void roomAddShelf(Shelf shelf) {
+        shelfList.add(shelf);
+    }
+
+    public void roomRemoveShelf(Shelf shelf) {
+        shelfList.remove(shelf);
+    }
+
+    //needed for tests
+    public void setRoomID(UUID roomID) {
+        this.roomID = roomID;
+    }
+
+    public void setShelfList(List<Shelf> shelfList) {
+        this.shelfList = shelfList;
+    }
+
+    public Room() {
+        this.shelfList = new ArrayList<Shelf>();
+        this.shelfLimit = 5;
+        this.roomID = UUID.randomUUID();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Room room = (Room) o;
+        return shelfLimit == room.shelfLimit && Objects.equals(shelfList, room.shelfList) && Objects.equals(roomID, room.roomID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(shelfList, shelfLimit, roomID);
+    }
 }
--- src/test/java/hwr/oop/BookTest.java
+++ src/test/java/hwr/oop/BookTest.java
@@ -3,11 +3,15 @@ package hwr.oop;
 import org.junit.jupiter.api.Test;
 import org.assertj.core.api.Assertions;
 
+import java.util.UUID;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookTest {
     @Test
-    void createBook_checkRightAttributes(){
+    void createBook_checkRightAttributes() {
         Room room = new Room();
-        Shelf shelf = new Shelf(room, "Action", 400,1);
+        Shelf shelf = new Shelf(room, "Action", 400, 1);
         Book book = new Book("Welt", "Peter Hans", "Natur", shelf, 100);
         Assertions.assertThat(book.getBookID()).isNotNull();
         Assertions.assertThat(book.getBookTitle()).isEqualTo("Welt");
@@ -16,10 +20,11 @@ public class BookTest {
         Assertions.assertThat(book.getShelf()).isEqualTo(shelf);
         Assertions.assertThat(book.getBookCondition()).isEqualTo(100);
     }
+
     @Test
-    void borrowBook_checkIfBorrowedByIsSetToGivenVisitorAndShelfIsNull(){
+    void borrowBook_checkIfBorrowedByIsSetToGivenVisitorAndShelfIsNull() {
         Room room = new Room();
-        Shelf shelf = new Shelf(room, "Action", 400,1);
+        Shelf shelf = new Shelf(room, "Action", 400, 1);
         Book book = new Book("Welt", "Peter Hans", "Natur", shelf, 100);
         Visitor visitor = new Visitor("Max", "Mustermann", "01.01.1999", "max.mustermann@gmx.de");
         book.borrow(visitor);
@@ -28,10 +33,11 @@ public class BookTest {
         Assertions.assertThat(book.getShelf()).isNull();
         Assertions.assertThat(book).isNotIn(shelf.getBooksOnShelf());
     }
+
     @Test
-    void returnBook_checkIfShelfIsSetToGivenShelfAndBorrowedByIsNull(){
+    void returnBook_checkIfShelfIsSetToGivenShelfAndBorrowedByIsNull() {
         Room room = new Room();
-        Shelf shelf = new Shelf(room, "Action", 400,1);
+        Shelf shelf = new Shelf(room, "Action", 400, 1);
         Book book = new Book("Welt", "Peter Hans", "Natur", shelf, 100);
         Visitor visitor = new Visitor("Max", "Mustermann", "01.01.1999", "max.mustermann@gmx.de");
         book.borrow(visitor);
@@ -41,4 +47,46 @@ public class BookTest {
         Assertions.assertThat(book.getShelf()).isEqualTo(shelf);
         Assertions.assertThat(book).isIn(shelf.getBooksOnShelf());
     }
+
+    @Test
+    void testEqualsMethod() {
+        Room room1 = new Room();
+        Room room2 = new Room();
+        Room room3 = new Room();
+
+        //Comparison with null should be return false
+        Assertions.assertThat(room1).isNotEqualTo(null);
+
+        //Comparison with an object of another class should be return false
+        Assertions.assertThat(room1).isNotEqualTo(new Shelf(room1, "Action", 400, 1));
+
+        List<Shelf> shelfList = new ArrayList<>();
+        shelfList.add(new Shelf(room3, "Fiction", 200, 1));
+        shelfList.add(new Shelf(room3, "Science", 100, 2));
+        room3.setShelfList(shelfList);
+        Assertions.assertThat(room3.getShelfList()).isEqualTo(shelfList);
+
+        Assertions.assertThat(room1.equals(room2)).isFalse();
+
+        UUID roomID = room1.getRoomID();
+        List<Shelf> shelfList2 = room1.getShelfList();
+        room2.setRoomID(roomID);
+        room2.setShelfList(shelfList2);
+        Assertions.assertThat(room1.equals(room2)).isTrue();
+    }
+
+    @Test
+    void testHashCodeMethod() {
+        Room room1 = new Room();
+        Room room2 = new Room();
+
+        Assertions.assertThat(room1.hashCode()).isNotEqualTo(room2.hashCode());
+
+        UUID roomID = room1.getRoomID();
+        List<Shelf> shelfList2 = room1.getShelfList();
+        room2.setRoomID(roomID);
+        room2.setShelfList(shelfList2);
+        Assertions.assertThat(room1).hasSameHashCodeAs(room2);
+    }
+
 }
