c82b454d5040d5e2ed50d861f645fe21924949a4
Ahktuti99 <a.elamri04@gmail.com>
2024-04-15 10:32:17 +0200

    deleted not needed lines of code for visitorTest

--- src/test/java/hwr/oop/VisitorTest.java
+++ src/test/java/hwr/oop/VisitorTest.java
@@ -92,9 +92,6 @@ class VisitorTest{
         // Create Visitor object with different attributes
         Visitor visitor3 = new Visitor("Jane", "Doe", "1990-01-01", "jane.doe@example.com");
 
-        // Ensure equals method works correctly for identical visitors
-        Assertions.assertThat(visitor1).isEqualTo(visitor1);
-
         // Ensure equals method works correctly for different visitors
         Assertions.assertThat(visitor1).isNotEqualTo(visitor3);
 
@@ -103,9 +100,6 @@ class VisitorTest{
         books1.add(new Book("Welt", "Peter Hans", "Natur",new Shelf(new Room(), "Acrion", 400, 1),100));
         visitor3.setBooksToReturn(books1);
         Assertions.assertThat(visitor3.getBooksToReturn()).isEqualTo(books1);
-
-        // Ensure equals method works correctly after modifying attributes
-        Assertions.assertThat(visitor1.equals(visitor2)).isFalse();
 
         // Ensure equals method works correctly after setting same attributes
         UUID setvisiorID = visitor1.getVisitorID();
@@ -127,18 +121,17 @@ class VisitorTest{
         // Create Visitor object with same attributes as visitor1
         Visitor visitor2 = new Visitor("John", "Doe", "1990-01-01", "john.doe@example.com");
 
-        // Create Visitor object with different attributes
-        Visitor visitor3 = new Visitor("Jane", "Doe", "1990-01-01", "jane.doe@example.com");
+        // Ensure hashCode method returns different values for different visitors | visitor1 and visitor2 have different UUID´s
+        Assertions.assertThat(visitor1.hashCode()).isNotEqualTo(visitor2.hashCode());
 
-        // Ensure hashCode method returns the same value for identical visitors
+        // Ensure hashCode method returns the same value for identical visitors | setting UUID from visitor1 to visitor2
         UUID setvisitorID = visitor1.getVisitorID();
         visitor2.setVisitorID(setvisitorID);
         visitor2.setBorrowedBooks(visitor1.getBorrowedBooks());
         visitor2.setBooksToReturn(visitor1.getBooksToReturn());
         Assertions.assertThat(visitor1).hasSameHashCodeAs(visitor2);
 
-        // Ensure hashCode method returns different values for different visitors
-        Assertions.assertThat(visitor1.hashCode()).isNotEqualTo(visitor3.hashCode());
+
     }
 
 
